3cbf25f19c8cdf889aed3de72dfd90ea2935e4ef
Blockaroz <lbpk.123fan@gmail.com>
2022-08-20 20:41:53 -0400

    Initial commit

---
+++ Content/ParticleLoader.cs
@@ -0,0 +1,31 @@
+﻿using On.Terraria;
+using Terraria;
+
+namespace ParticleEngine
+{
+    public static class ParticleLoader
+    {
+        public static void Load()
+        {
+            On.Terraria.Main.DrawDust += DrawParticles;
+            On.Terraria.Main.UpdateParticleSystems += UpdateParticles;
+        }
+
+        public static void Unload()
+        {
+            ParticleSystem.Unload();
+        }
+
+        private static void DrawParticles(On.Terraria.Main.orig_DrawDust orig, Terraria.Main self)
+        {
+            orig(self);
+            ParticleSystem.DrawParticles(Terraria.Main.spriteBatch);
+        }
+
+        private static void UpdateParticles(On.Terraria.Main.orig_UpdateParticleSystems orig, Terraria.Main self)
+        {
+            orig(self);
+            ParticleSystem.UpdateParticles();
+        }
+    }
+}
---
+++ Content/ParticleSystem.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+
+namespace ParticleEngine
+{
+    public static class ParticleSystem
+    {
+        private static int nextID;
+
+        internal static readonly IList<Particle> particleTypes = new List<Particle>();
+
+        public static IList<Particle> particle = new List<Particle>();
+
+        internal static int ReserveParticleID() => nextID++;
+
+        public static Particle GetParticle(int type) => (type == -1) ? null : particleTypes[type];
+
+        internal static void Unload()
+        {
+            particleTypes.Clear();
+            nextID = 0;
+        }
+
+        public static void UpdateParticles()
+        {
+            if (Main.dedServ || Main.gamePaused)
+            {
+                return;
+            }
+
+            foreach (Particle item in particle.ToList())
+            {
+                item.position += item.velocity;
+                item.Update();
+                if (!item.Active)
+                    particle.Remove(item);
+
+            }
+        }
+
+        public static void DrawParticles(SpriteBatch spriteBatch)
+        {
+            if (Main.dedServ)
+                return;
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
+            Rectangle value = new Rectangle((int)Main.screenPosition.X - Main.screenWidth, (int)Main.screenPosition.Y - Main.screenHeight, Main.screenWidth * 2, Main.screenHeight * 2);
+
+            foreach (Particle particle in particle.Where((Particle p) => p.shader == null))
+            {
+                if (new Rectangle((int)particle.position.X - 3, (int)particle.position.Y - 3, 6, 6).Intersects(value) && Main.netMode != 2)
+                {
+                    particle.Draw(spriteBatch);
+                }
+            }
+
+            foreach (Particle item2 in particle.Where((Particle p) => p.shader != null))
+            {
+                if (new Rectangle((int)item2.position.X - 3, (int)item2.position.Y - 3, 6, 6).Intersects(value) && Main.netMode != NetmodeID.Server)
+                {
+                    item2.emit = false;
+                    item2.shader.Apply(null);
+                    item2.Draw(spriteBatch);
+                    Main.pixelShader.CurrentTechnique.Passes[0].Apply();
+                }
+            }
+
+            spriteBatch.End();
+        }
+    }
+}
